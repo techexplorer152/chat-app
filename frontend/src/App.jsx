fb67a2558669008f8370949ba348416f8932eda5
MATHMAN7 <159688574+MATHMAN7@users.noreply.github.com>
2025-10-22 23:46:57 +0200

    Made everything ready to start working on this project :)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>HI</div>
+  )
+}
+
+export default App
