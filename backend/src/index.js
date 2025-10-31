3d9d1adea34683f394d94e0d46b883d16f8772a0
MATHMAN7 <159688574+MATHMAN7@users.noreply.github.com>
2025-10-31 13:04:00 +0100

    Set up Express server and added auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json())
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
 
