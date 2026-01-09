6b512d4916a7c9361b236102b7c25ac6901127eb
techexplorer152 <sonycena844@gmail.com>
2026-01-09 23:38:46 +0100

    life is good but my app is sick

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -4,7 +4,11 @@ import { upload } from '../middleware/upload.middleware.js';
 
 const router = express.Router();
 
+
 router.get('/', getAllMessages);
+
+
+router.post('/text', createMessage);
 
 
 router.post('/', upload.single('image'), createMessage);
--- backend/src/services/message.service.js
+++ backend/src/services/message.service.js
@@ -1,16 +1,17 @@
 import prisma from '../db/connect.js';
 
-
-
 export async function saveGroupMessage({ text, image_url, sender_id }) {
-    return await prisma.messages.create({
-        data: { text, image_url, sender_id },
+    return prisma.messages.create({
+        data: {
+            text,
+            image_url,
+            sender_id,
+        },
     });
 }
 
-
 export async function getAllGroupMessages() {
-    return await prisma.messages.findMany({
+    return prisma.messages.findMany({
         orderBy: { created_at: 'asc' },
     });
 }
