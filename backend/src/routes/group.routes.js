05c9d4bf29d07caa039906fa03cc0974f39e541c
techexplorer152 <sonycena844@gmail.com>
2026-03-04 23:48:32 +0100

    routes update

--- backend/src/routes/group.routes.js
+++ backend/src/routes/group.routes.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { createGroup, getMyGroups, joinGroup, addUser } from "../controllers/group.controller.js";
+import {
+    createGroup,
+    getMyGroups,
+    joinGroup,
+    addUser,
+    removeMember,
+    deleteGroup
+} from "../controllers/group.controller.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,8 +14,9 @@ const router = express.Router();
 router.get("/me", authenticate, getMyGroups);
 router.post("/", authenticate, createGroup);
 router.post("/join/:groupId", authenticate, joinGroup);
-
-
 router.post("/:groupId/add", authenticate, addUser);
+
+router.delete("/:groupId/members/:userId", authenticate, removeMember);
+router.delete("/:groupId", authenticate, deleteGroup);
 
 export default router;
