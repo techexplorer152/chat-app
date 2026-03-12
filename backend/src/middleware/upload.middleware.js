2e901885145b21cd46a3df102c9c1d2e04211739
techexplorer152 <sonycena844@gmail.com>
2026-03-12 17:10:45 +0100

    API fix

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -1,12 +1,20 @@
 import multer from 'multer';
-import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'portfolio_uploads',
+        allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
     },
 });
 
